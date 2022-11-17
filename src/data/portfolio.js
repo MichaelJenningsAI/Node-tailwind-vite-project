1dfc9983aba2a9c9e5c1a2c9157f7ebd3debf35e
Michael Jennings <michaeljenningsai@gmail.com>
2022-11-17 20:04:18 +1100

    Test

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Project 1',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://youtube.com/mjai'
+    },
+    {
+        title: 'Project 2',
+        imgUrl: '',
+        stack: ['Python', 'CSS', 'JavaScript'],
+        link: 'https://youtube.com/mjai'
+    },
+    {
+        title: 'Project 3',
+        imgUrl: '',
+        stack: ['test', 'CSS', 'JavaScript'],
+        link: 'https://youtube.com/mjai'
+    },
+]
