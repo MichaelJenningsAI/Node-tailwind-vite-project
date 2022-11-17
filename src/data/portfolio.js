68246198bdf33aee10475166425328c75ddc6e7c
Michael Jennings <michaeljenningsai@gmail.com>
2022-11-18 09:43:06 +1100

    rickrolled

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,18 +3,18 @@ export default [
         title: 'Python Stuff',
         imgUrl: '',
         stack: ['HTML', 'CSS', 'JavaScript'],
-        link: 'https://youtube.com/mjai'
+        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     },
     {
         title: 'Java Stuff',
         imgUrl: '',
         stack: ['Python', 'CSS', 'JavaScript'],
-        link: 'https://youtube.com/mjai'
+        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     },
     {
         title: 'Web Stuff',
         imgUrl: '',
         stack: ['test', 'CSS', 'JavaScript'],
-        link: 'https://youtube.com/mjai'
+        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     },
 ]
