bffa926319e3cac86bbc64a9d1d497d96bdbc792
Michael Jennings <michaeljenningsai@gmail.com>
2022-11-18 18:48:11 +1100

    Revert "rickrolled"
    This reverts commit 68246198bdf33aee10475166425328c75ddc6e7c.

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,19 +3,19 @@ export default [
         title: 'Python Stuff',
         imgUrl: '',
         stack: ['HTML', 'CSS', 'JavaScript'],
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        link: 'https://youtube.com/mjai'
     },
     {
         title: 'Java Stuff',
         imgUrl: '',
         stack: ['Python', 'CSS', 'JavaScript'],
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        link: 'https://youtube.com/mjai'
     },
     {
         title: 'Web Stuff',
         imgUrl: '',
         stack: ['test', 'CSS', 'JavaScript'],
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        link: 'https://youtube.com/mjai'
     },
     {
         title: 'Ana',
